f291f42c7c240a7f2bc48b207ea8743f5d8a8315
Lance Boylan <eg01st3220@gmail.com>
2026-04-01 16:12:37 -0600

    Add project files.

---
+++ o31/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> names = new() { "mage", "warrior", "rogue" };
+        names.Select(x => x.ToUpper()).ToList();
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+        var nameLength = names.Select(y => y.Count()).ToList();
+        
+        foreach (int nameCount in nameLength)
+        {
+            Console.WriteLine(nameCount);
+        }
+    }
+}
